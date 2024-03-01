8d539703dfb897eb7b78015b9ffd080781bda264
Md Adil Khan <md.adil.khan.@longshortlabs.com>
2024-03-01 12:38:31 +0530

    handled cors error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 // https://eshop-tutorial-pyri.vercel.app
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://eshop-bc5b4.web.app",
   credentials: true
 }));
 
